2bdd2bef07ca1b2c4707e055861e52bc3fe38c31
x-wei <wistar.sjtu@gmail.com>
2020-04-19 11:56:31 +0200

    add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+echo "REPO: $GITHUB_REPOSITORY"
+echo "ACTOR: $GITHUB_ACTOR"
+
+echo '=================== Setting locale ==================='
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+    dpkg-reconfigure --frontend=noninteractive locales && \
+    update-locale LANG=en_US.UTF-8
+
+locale-gen zh_CN.UTF-8 && \
+    locale-gen zh_HK.UTF-8 && \
+    locale-gen en_US.UTF-8 && \
+    locale-gen ja_JP.UTF-8 && \
+    locale -a
+
+echo '=================== Building blog ==================='
+cd pelican_dir && make publish
+
+# TODO
+# echo '=================== Publish to GitHub Pages ==================='
+# remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+# remote_branch=${GH_PAGES_BRANCH:=gh-pages}
+# git init
+# git remote add deploy "$remote_repo"
+# git checkout $remote_branch || git checkout --orphan $remote_branch
+# git config user.name "${GITHUB_ACTOR}"
+# git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+# git add .
+# echo -n 'Files to Commit:' && ls -l | wc -l
+# timestamp=$(date +%s%3N)
+# git commit -m "[ci skip] Automated deployment to GitHub Pages on $timestamp"
+# git push deploy $remote_branch --force
+# rm -fr .git
+# cd ../
+# echo '=================== Done  ==================='
