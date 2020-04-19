8cbc37c8b0ea0b5ff7dbc02b9e3a345ed9bc4f6d
x-wei <wistar.sjtu@gmail.com>
2020-04-19 15:32:16 +0200

    enable pushing to gh-pages branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# exit when any command fails
 set -e
 
 echo "REPO: $GITHUB_REPOSITORY"
@@ -19,20 +20,20 @@ locale-gen zh_CN.UTF-8 && \
 echo '=================== Building blog ==================='
 cd pelican_dir && make publish
 
-# TODO
-# echo '=================== Publish to GitHub Pages ==================='
-# remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-# remote_branch=${GH_PAGES_BRANCH:=gh-pages}
-# git init
-# git remote add deploy "$remote_repo"
-# git checkout $remote_branch || git checkout --orphan $remote_branch
-# git config user.name "${GITHUB_ACTOR}"
-# git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-# git add .
-# echo -n 'Files to Commit:' && ls -l | wc -l
-# timestamp=$(date +%s%3N)
-# git commit -m "[ci skip] Automated deployment to GitHub Pages on $timestamp"
-# git push deploy $remote_branch --force
-# rm -fr .git
-# cd ../
-# echo '=================== Done  ==================='
+
+echo '=================== Publish to GitHub Pages ==================='
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_branch=${GH_PAGES_BRANCH:=gh-pages}
+git init
+git remote add deploy "$remote_repo"
+git checkout $remote_branch || git checkout --orphan $remote_branch
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+echo -n 'Files to Commit:' && ls -l | wc -l
+timestamp=$(date +%s%3N)
+git commit -m "[ci skip] Automated deployment to GitHub Pages on $timestamp"
+git push deploy $remote_branch --force
+rm -fr .git
+cd ../
+echo '=================== Done  ==================='
